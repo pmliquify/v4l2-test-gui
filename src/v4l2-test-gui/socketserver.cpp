9ea597a911b4af6bbe03725eb92f41c70c8fee21
Peter Martienssen <peter.martienssen@liquify-consulting.de>
2025-03-08 22:52:44 +0100

    SocketServer::readDate now reads as many data as possible

--- src/v4l2-test-gui/socketserver.cpp
+++ src/v4l2-test-gui/socketserver.cpp
@@ -56,46 +56,48 @@ void SocketServer::readData()
 {
     QTcpSocket* clientSocket = qobject_cast<QTcpSocket*>(sender());
     if (clientSocket) {
-        if (!m_headerReceived) {
-            if (clientSocket->bytesAvailable() < sizeof(ImageHeader)) {
-                return; 
-            }
-            m_headerReceived = true;
-
-            QByteArray data = clientSocket->read(sizeof(ImageHeader));
-            ImageHeader* header = reinterpret_cast<ImageHeader*>(data.data());
-            if (m_image->imageSize() != header->imageSize) {
-                if (m_image->imageSize() != 0) {
-                        for (int index=0; index<m_image->planes().size(); index++) {
-                                free(m_image->planes().at(index));
-                        }
+        while (true){
+            if (!m_headerReceived) {
+                if (clientSocket->bytesAvailable() < sizeof(ImageHeader)) {
+                    return; 
                 }
-                m_image->planes().resize(header->numPlanes);
-                m_image->setImageSize(header->imageSize);
-                for (int index=0; index<m_image->planes().size(); index++) {
-                        m_image->planes()[index] = (unsigned char *)malloc(m_image->imageSize());
+                m_headerReceived = true;
+                
+                QByteArray data = clientSocket->read(sizeof(ImageHeader));
+                ImageHeader* header = reinterpret_cast<ImageHeader*>(data.data());
+                if (m_image->imageSize() != header->imageSize) {
+                    if (m_image->imageSize() != 0) {
+                            for (int index=0; index<m_image->planes().size(); index++) {
+                                    free(m_image->planes().at(index));
+                            }
+                    }
+                        m_image->planes().resize(header->numPlanes);
+                        m_image->setImageSize(header->imageSize);
+                    for (int index=0; index<m_image->planes().size(); index++) {
+                            m_image->planes()[index] = (unsigned char *)malloc(m_image->imageSize());
+                    }
                 }
-            }
 
-            m_image->init(header->width, header->height, header->bytesPerLine, header->imageSize,
-                header->bytesUsed, header->pixelformat, header->sequence, header->timestamp);
-            m_image->setShift(header->shift);
+                m_image->init(header->width, header->height, header->bytesPerLine, header->imageSize,
+                    header->bytesUsed, header->pixelformat, header->sequence, header->timestamp);
+                m_image->setShift(header->shift);
 
-        } else {
-            unsigned int receiveSize = (m_image->bytesUsed() > 0) ? m_image->bytesUsed() : m_image->imageSize();
-            if (clientSocket->bytesAvailable() < receiveSize) {
-                return; 
-            }
-            m_headerReceived = false;
+            } else {
+                unsigned int receiveSize = (m_image->bytesUsed() > 0) ? m_image->bytesUsed() : m_image->imageSize();
+                if (clientSocket->bytesAvailable() < receiveSize) {
+                    return; 
+                }
+                m_headerReceived = false;
 
-            unsigned char *plane = (unsigned char *)m_image->planes()[0];
-            int size = clientSocket->read((char *)plane, receiveSize);
-            if (size < 0) {
-                clientSocket->close();
-                return;
+                unsigned char *plane = (unsigned char *)m_image->planes()[0];
+                int size = clientSocket->read((char *)plane, receiveSize);
+                if (size < 0) {
+                    clientSocket->close();
+                    return;
+                }
+                
+                emit imageReceived(*m_image);
             }
-            
-            emit imageReceived(*m_image);
         }
     }
 }
